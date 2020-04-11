2ca9cc785b96d508b95c66c1ca21a41f1cf9f2fd
MechaDragonX <theofficialrv@gmail.com>
2020-04-11 10:55:01 -0700

    Create new project for GUI desktop application

---
+++ Majora.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Majora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
