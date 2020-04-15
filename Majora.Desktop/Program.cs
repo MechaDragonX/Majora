48f7a48a97247d41d3e10e44c995cc1c23e9d820
MechaDragonX <theofficialrv@gmail.com>
2020-04-15 12:11:44 -0700

    Restart desktop application with Avalonia

--- Majora.Desktop/Program.cs
+++ Majora.Desktop/Program.cs
@@ -1,31 +1,22 @@
-﻿using Chromely.Core;
-using Chromely.Core.Configuration;
-using Chromely.Core.Infrastructure;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Logging.Serilog;
 
-namespace Majora
+namespace Majora.Desktop
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var config = DefaultConfiguration.CreateForRuntimePlatform();
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
 
-            config.AppName = "Majora";
-            config.StartUrl = "local://App/index.html";
-            config.WindowOptions.Title = "Majora";
-            config.WindowOptions.RelativePathToIconFile = "local://Assets/majora.ico";
-#if DEBUG
-            config.DebuggingMode = true;
-#elif RELEASE
-            config.DebuggingMode = false;
-#endif
-
-            AppBuilder
-            .Create()
-            .UseApp<Majora>()
-            .UseConfiguration<IChromelyConfiguration>(config)
-            .Build()
-            .Run(args);
-        }
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
     }
 }
